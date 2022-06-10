c114fdfad52f6e0e5be6f2a9f80745da6c602c44
amos18g <107020578+amos18g@users.noreply.github.com>
2022-06-09 18:12:14 -0600

    Update server.go

--- server/server.go
+++ server/server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	mA "control/manejo_archivos"
+	conFiles "control/files_controller"
 	"fmt"
 	"net"
 	"strconv"
@@ -14,30 +14,30 @@ const (
 
 	SND = "send"
 
-	CLIENTE = "client "
+	CLT = "client "
 )
 
-var contadorCliente = 1
-var bandera = false
+var counterClient = 1
+var status = false
 
-type clienteStruct struct {
-	nombre    string
-	direccion string
-	conn      net.Conn
-	channel   int
+type clientStruct struct {
+	name    string
+	add     string
+	conn    net.Conn
+	channel int
 }
 
-func (this *clienteStruct) DetalleCliente(nombre string, conn net.Conn, direccion string) {
-	this.nombre = nombre
-	this.conn = conn
-	this.direccion = direccion
+func (this *clientStruct) detailClient(name1 string, conn1 net.Conn, address1 string) {
+	this.name = name1
+	this.conn = conn1
+	this.add = address1
 }
 
-var clients []clienteStruct
+var clients []clientStruct
 
 func main() {
 
-	//inicia server
+	//start server
 	server, err := net.Listen("tcp", "127.0.0.1:8000")
 
 	if err != nil {
@@ -47,40 +47,36 @@ func main() {
 	fmt.Println()
 
 	for {
-		//se conecta un cliente
+		//a client connects
 
 		client, err := server.Accept()
 		if err != nil {
-			fmt.Printf("Se ha producido un error: %v \n", err)
+			fmt.Printf("An error has occurred: %v \n", err)
 			return
 		}
-		cliente1 := new(clienteStruct)
+		client1 := new(clientStruct)
 
 		if err != nil {
-			fmt.Printf("Se ha producido un error: %v \n", err)
+			fmt.Printf("An error has occurred: %v \n", err)
 			return
 		}
 
-		if bandera {
-			actualClient := CLIENTE + strconv.Itoa(contadorCliente)
-			cliente1.DetalleCliente(actualClient, client, client.RemoteAddr().String())
-			clients = append(clients, *cliente1)
-			contadorCliente++
+		if status {
+			currentClient := CLT + strconv.Itoa(counterClient)
+			client1.detailClient(currentClient, client, client.RemoteAddr().String())
+			clients = append(clients, *client1)
+			counterClient++
 		} else {
-			actualClient := CLIENTE + strconv.Itoa(contadorCliente)
-			cliente1.DetalleCliente(actualClient, client, client.RemoteAddr().String())
-			clients = append(clients, *cliente1)
-			bandera = true
-			contadorCliente++
+			currentClient := CLT + strconv.Itoa(counterClient)
+			client1.detailClient(currentClient, client, client.RemoteAddr().String())
+			clients = append(clients, *client1)
+			status = true
+			counterClient++
 		}
 
 		go listenClients(client)
 
 	}
-
-	//mA.PreparEnvio(client, "./algo.txt")
-
-	//go managerConnection(client)
 
 }
 
@@ -88,72 +84,78 @@ func listenClients(client net.Conn) {
 	for {
 		var buff = make([]byte, 2048)
 		n, err := client.Read(buff)
-		dirClientAc := client.RemoteAddr().String()
 
 		if err != nil {
 
 			return
 		}
 
-		//se ejecuta una vez cada vez que se hace enter
-		//var texto = scanner.Text()
-		var texto = string(buff[:n])
-		args := strings.Split(texto, " ")
+		var text = string(buff[:n])
 
-		switch args[0] {
-		case CH:
-			var canal int
-			canal, _ = strconv.Atoi(args[1])
-			for i, cs := range clients {
-				if cs.conn.RemoteAddr().String() == dirClientAc {
-					clients[i].channel = canal
-					//texto en consola client subscrito
-					fmt.Printf("./%s subscribed -channel %d\n\n", clients[i].nombre, clients[i].channel)
-
-					break
-				}
-			}
-
-		case SND:
-			nameFile := args[1]
-			canal, _ := strconv.Atoi(args[2])
-			canalNum := args[2]
-
-			//./client send miarchivo.png -channel 1
-			var clientSend string
-			for i, cs := range clients {
-				if client.RemoteAddr().String() == cs.conn.RemoteAddr().String() {
-					//fmt.Printf("./%s send %s -channel %d\n", clients[i].nombre, nameFile, canal)
-					clientSend = "./" + clients[i].nombre + " send " + nameFile + "-channel " + canalNum
-				}
-
-				if client.RemoteAddr().String() != cs.conn.RemoteAddr().String() && cs.channel == canal {
-					fmt.Printf("./%s receive -channel %d\n\n", clients[i].nombre, canal)
-
-				}
-			}
-			fmt.Println(clientSend)
-			fmt.Println()
-
-			nameFile = "./" + nameFile
-
-			mA.RecibirArchivo(client, nameFile)
-
-			writeMessageAllUsers(nameFile, client, canal)
-
-		default:
-			fmt.Printf("wrong entry: %s\n", texto)
-		}
+		optionServer(client, text)
 
 	}
+
 }
 
-func writeMessageAllUsers(ruta string, conn net.Conn, canal int) {
-	
-	for i, _ := range clients {
-		if (clients[i].conn.RemoteAddr().String() != conn.RemoteAddr().String()) && clients[i].channel == canal {
-			clients[i].conn.Write([]byte(ruta))
-			mA.EnviarArchivo(clients[i].conn, ruta)
+func optionServer(client net.Conn, text string) {
+	dirClientCurrent := client.RemoteAddr().String()
+
+	args := strings.Split(text, " ")
+
+	switch args[0] {
+	case CH:
+		var canal int
+		canal, _ = strconv.Atoi(args[1])
+		for i, cs := range clients {
+			if cs.conn.RemoteAddr().String() == dirClientCurrent {
+				clients[i].channel = canal
+				//texto en consola client subscrito
+				fmt.Printf("./%s subscribed -channel %d\n\n", clients[i].name, clients[i].channel)
+
+				break
+			}
+		}
+
+	case SND:
+		nameFile := args[1]
+		channel, _ := strconv.Atoi(args[2])
+		channelNum := args[2]
+
+		//./client send miarchivo.png -channel 1
+		var clientSend string
+		for i, cs := range clients {
+			if dirClientCurrent == cs.conn.RemoteAddr().String() {
+				//fmt.Printf("./%s send %s -channel %d\n", clients[i].nombre, nameFile, canal)
+				clientSend = "./" + clients[i].name + " send " + nameFile + "-channel " + channelNum
+			}
+
+			if dirClientCurrent != cs.conn.RemoteAddr().String() && cs.channel == channel {
+				fmt.Printf("./%s receive -channel %d\n\n", clients[i].name, channel)
+
+			}
+		}
+		fmt.Println(clientSend)
+		fmt.Println()
+
+		nameFile = "./" + nameFile
+
+		//conFiles.R(client, nameFile)
+		conFiles.ReceiveFiles(client, nameFile)
+		writeFilesAllUsers(nameFile, client, channel)
+
+	default:
+		fmt.Printf("wrong entry: %s\n", text)
+	}
+
+}
+
+func writeFilesAllUsers(path string, conn net.Conn, channel int) {
+
+	for i := range clients {
+		if (clients[i].conn.RemoteAddr().String() != conn.RemoteAddr().String()) && clients[i].channel == channel {
+			clients[i].conn.Write([]byte(path))
+			conFiles.SendFiles(clients[i].conn, path)
 		}
 
 	}
